4d2bbb15d457b3e39cad20b39d7120da3962c3dc
wangchenghao <wangchenghao@58.com>
2020-02-26 15:57:11 +0800

    +1 test

--- app/src/main/java/com/example/basictest/MainActivity.java
+++ app/src/main/java/com/example/basictest/MainActivity.java
@@ -1,21 +1,23 @@
 package com.example.basictest;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MY_TEST", "onCreate1");
+        Log.i(TAG, "onCreate: +1");
     }
 
     @Override
