90d4a2c9ed6fc7e0bd9256f6def6b4f508041ada
wangchenghao <wangchenghao@58.com>
2020-03-06 17:32:14 +0800

    清理代码

--- app/src/main/java/com/example/basictest/MainActivity.java
+++ app/src/main/java/com/example/basictest/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,10 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.i("MY_TEST", "onResume1");
-        Log.i(TAG, "onResume: " + "添加了第二行代码，准备commit");
-        Log.i(TAG, "onResume: " + "添加了第二行代码，准备第二次commit");
-
-
     }
 
     @Override
