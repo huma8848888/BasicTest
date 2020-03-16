bcdf8c5e81532695f10b8909674b3098aaad2410
wangchenghao <wangchenghao@58.com>
2020-03-16 19:21:48 +0800

    2

--- app/src/main/java/com/example/basictest/MainActivity2.java
+++ app/src/main/java/com/example/basictest/MainActivity2.java
@@ -16,7 +16,7 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Log.d(TAG, "onCreate: 1");
-
+        Log.d(TAG, "onCreate: 2");
     }
 
     @Override
