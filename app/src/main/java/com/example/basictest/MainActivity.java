b30456fb974acd280ef81dd1b278a66a153ecf7c
wangchenghao <jonsonwangchenghao@didichuxing.com>
2020-11-20 15:21:55 +0800

    删除

--- app/src/main/java/com/example/basictest/MainActivity.java
+++ app/src/main/java/com/example/basictest/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MY_TEST", "onCreate1");
-        Log.i(TAG, "onCreate: start");
-        Log.i(TAG, "onCreate: 1 commit!!!");
-        Log.i(TAG, "onCreate: 3 commit!!!");
 
     }
 
