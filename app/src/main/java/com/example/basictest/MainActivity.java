8835ffe5c4784234e9639ca6ee71e0e4560b25e6
wangchenghao <jonsonwangchenghao@didichuxing.com>
2021-02-05 17:53:39 +0800

    master commit2

--- app/src/main/java/com/example/basictest/MainActivity.java
+++ app/src/main/java/com/example/basictest/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MY_TEST", "onCreate1");
+        Log.i(TAG, "onCreate: master commit 2");
     }
 
     @Override
