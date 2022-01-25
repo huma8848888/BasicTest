18f53c6b8c688e4291b8012cbb938ca72ea98f29
wangchenghao <329887179@qq.com>
2022-01-26 00:31:55 +0800

    +2

--- app/src/main/java/com/example/basictest/MainActivity.java
+++ app/src/main/java/com/example/basictest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.basictest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
@@ -12,11 +14,21 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    static class MyHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    }
+    Handler handler = new MyHandler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String str = "abc";
         Log.i("MY_TEST", "onCreate1");
+        Log.i("MY_TEST", "onCreate2");
     }
 
     @Override
